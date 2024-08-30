5d54681a2d9f1ade95c0f9b43f6424004b6d664f
root <root@debian-12>
2024-08-30 16:03:59 +0000

    encoded for debian - python 311 - 2024-08-30 16:03:59

--- pyarmor_runtime_000000/__init__.py
+++ pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 8.5.11 (trial), 000000, 2024-08-27T10:01:17.293604
+# Pyarmor 8.5.11 (trial), 000000, 2024-08-30T16:03:55.747668
 from .pyarmor_runtime import __pyarmor__
--- static/pages/plans.js
+++ static/pages/plans.js
@@ -58,6 +58,15 @@ initialModalTitleContent = document.querySelector(".modal-header").innerHTML;
 document.getElementById("CreatePlanButton").addEventListener("click", function () {
 
 
+    // Set custom iamge name from field
+    const customRadioButton = document.querySelector('input[name="docker_image"][value="custom"]');
+    if (customRadioButton.checked) {
+        const customImageName = document.getElementById("custom_image_name").value;
+        customRadioButton.value = customImageName;
+    }
+
+
+
     // Disable the button and show loading spinner
     var CreatePlanButton = document.getElementById("CreatePlanButton");
     CreatePlanButton.disabled = true;
