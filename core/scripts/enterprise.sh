7d6981ee279213eac2bf63106e558fee14784d9f
Stefan <stefan@pejcic.rs>
2025-04-17 14:37:56 +0200

    enkriptovano na s34 - python 3.12  - 2025-04-17 14:37:56

--- core/scripts/enterprise.sh
+++ core/scripts/enterprise.sh
@@ -2,4 +2,4 @@
 
 # upsell link for the enterprise
 
-ENTERPRISE_LINK="https://openpanel.com/product/openpanel-premium-control-panel/"
+ENTERPRISE_LINK="https://my.openpanel.com/index.php?rp=/store/openpanel/enterprise-license"
--- pyarmor_runtime_007078/__init__.py
+++ pyarmor_runtime_007078/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.0.4 (pro), 007078, 2025-04-11T14:21:32.095056
+# Pyarmor 9.0.4 (pro), 007078, 2025-04-17T14:37:46.553042
 from .pyarmor_runtime import __pyarmor__
