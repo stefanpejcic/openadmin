c1722f82fdafd741d09685736b4fd6163e7004b1
Stefan <stefan@pejcic.rs>
2025-05-15 13:32:04 +0200

    encoded for x86-64  - 2025-05-15 13:32:03

--- modules/api/generate.py
+++ modules/api/generate.py
@@ -4,7 +4,7 @@
 # * OpenAdmin                                                             *
 # * Copyright (c) OpenPanel. All Rights Reserved.                         *
 # * Version: 1.2.9                                                        *
-# * Build Date: 2025-05-15 12:58:36                                       *
+# * Build Date: 2025-05-15 13:32:03                                       *
 # *                                                                       *
 # *************************************************************************
 # *                                                                       *
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,12 @@ module.exports = {
   darkMode: 'class',
   content: [
     './templates/**/*.html',
-    './static/**/*.js'
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
+    },
   },
-}
+};
