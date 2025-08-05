ab8b2f8c7d5dfcea1de148d5713097e2cf728e4d
Stefan <stefan@pejcic.rs>
2025-08-05 15:47:24 +0200

    encoded for x86-64  - 2025-08-05 15:47:23

--- modules/api/generate.py
+++ modules/api/generate.py
@@ -4,7 +4,7 @@
 # * OpenAdmin                                                             *
 # * Copyright (c) OpenPanel. All Rights Reserved.                         *
 # * Version: 1.5.3                                                        *
-# * Build Date: 2025-08-04 10:25:51                                       *
+# * Build Date: 2025-08-05 15:47:23                                       *
 # *                                                                       *
 # *************************************************************************
 # *                                                                       *
