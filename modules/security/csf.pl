b3555b184267ebae835e5e100ba594168d6f8f84
Stefan <stefan@pejcic.rs>
2025-09-29 15:38:03 +0200

    encoded for x86-64  - 2025-09-29 15:38:02

--- modules/security/csf.pl
+++ modules/security/csf.pl
@@ -15,12 +15,12 @@ use Fcntl qw(:DEFAULT :flock);
 use Sys::Hostname qw(hostname);
 use IPC::Open3;
 use lib '/usr/local/csf/lib';
-use ConfigServer::DisplayUI;
-use ConfigServer::Config;
+use Sentinel::DisplayUI;
+use Sentinel::Config;
 
 our ($script, $images, $myv, %FORM, %in);
 
-my $config = ConfigServer::Config->loadconfig();
+my $config = Sentinel::Config->loadconfig();
 my %config = $config->config;
 
 open (my $IN, "<", "/etc/csf/version.txt");
@@ -63,7 +63,7 @@ unless ($FORM{action} eq "tailcmd" or $FORM{action} =~ /^cf/ or $FORM{action} eq
 <!doctype html>
 <html lang='en'>
 <head>
-	<title>ConfigServer Security &amp; Firewall</title>
+	<title>Sentinel Security &amp; Firewall</title>
 	<meta charset='utf-8'>
 	<meta name='viewport' content='width=device-width, initial-scale=1'>
 	$bootstrapcss
@@ -100,7 +100,7 @@ EOF
 <div class='container-fluid'>
 <br>
 <div class='panel panel-default'>
-<h4><img src='$images/csf_small.png' style='padding-left: 10px'> ConfigServer Security &amp; Firewall - csf v$myv</h4>
+<h4><img src='$images/csf_small.png' style='padding-left: 10px'> Sentinel Security &amp; Firewall - csf v$myv</h4>
 </div>
 EOF
 }
@@ -109,7 +109,7 @@ EOF
 #open (my $SCRIPTOUT, '>', \$templatehtml);
 #select $SCRIPTOUT;
 
-ConfigServer::DisplayUI::main(\%FORM, $script, $script, $images, $myv);
+Sentinel::DisplayUI::main(\%FORM, $script, $script, $images, $myv);
 
 #close ($SCRIPTOUT);
 #select STDOUT;
---
+++ static/pages/reseller_dashboard.js
@@ -0,0 +1,72 @@
+function updateUserActivityTable() {
+    $.ajax({
+        url: '/json/combined_activity',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            // Clear existing list items
+            $('#activity-list').empty();
+
+            if (data.combined_logs.length > 0) {
+                // Hide the placeholder
+                $('#shouldbehidden').hide();
+                
+                data.combined_logs.forEach(function (log) {
+                    var parts = log.split(' ');
+                    var date = parts.slice(0, 3).join(' ');
+                    var ip = parts[3];
+                    var userAndActivity = parts.slice(4).join(' ');
+                    var usernameMatch = parts[4] === 'Administrator' ? parts[5].match(/(\w+)/) : userAndActivity.match(/User (\w+)/);
+                    var username = usernameMatch ? usernameMatch[1] : '';
+                    var formattedDate = moment(date, 'YYYY-MM-DD HH:mm:ss').format('D.M.Y H:mm:ss');
+                    var now = moment();
+                    var isOnline = Math.abs(now.diff(moment(date, 'YYYY-MM-DD HH:mm:ss'), 'minutes')) <= 90;
+
+                    var avatarClass = parts[4] === 'Administrator' 
+                        ? 'bg-black dark:bg-black text-white' 
+                        : isOnline 
+                            ? 'bg-sky-500 dark:bg-sky-500 text-white' 
+                            : 'bg-gray-50 dark:bg-gray-800 text-gray-700 dark:text-white';
+
+                    var hreflink = parts[4] === 'Administrator' 
+                        ? '/administrators' 
+                        : `/users/${username}`;
+
+                    var avatarContent = parts[4] === 'Administrator' 
+                        ? '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-crown" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 6l4 6l5 -4l-2 10h-14l-2 -10l5 4z"></path></svg>' 
+                        : username[0].toUpperCase();
+
+                    var listItem = `
+                        <li class="flex flex-col items-center justify-between gap-4 pl-1 py-4 sm:flex-row sm:py-3 hover:bg-gray-50 hover:dark:bg-gray-900">
+                            <div class="flex w-full items-center gap-4">
+                                <a href="${hreflink}">
+                                    <span class="inline-flex size-9 items-center justify-center rounded-full ${avatarClass} p-1.5 text-xs font-medium ring-1 ring-gray-300 dark:ring-gray-700" aria-hidden="true">
+                                        ${avatarContent}
+                                    </span>
+                                </a>
+                                <div>
+                                    <p class="text-sm font-medium text-gray-900 dark:text-gray-50"><a href="/users/${username}">${ip}</a></p>
+                                    <p class="text-xs text-gray-600 dark:text-gray-400">${userAndActivity.replace(/user (\w+)/i, 'User <strong>$1</strong>')}</p>
+                                </div>
+                            </div>
+                            <div class="flex w-full items-center gap-3 sm:w-fit">
+                                <div class="text-xs text-gray-600 dark:text-gray-400">${formattedDate}</div>
+                            </div>
+                        </li>`;
+
+                    $('#activity-list').append(listItem);
+                });
+            } else {
+                $('#shouldbehidden').show();
+            }
+        },
+        error: function (error) {
+            console.error('Error fetching user activity:', error);
+        }
+    });
+}
+
+// --- Init ---
+$(function () {
+    updateUserActivityTable();
+});
