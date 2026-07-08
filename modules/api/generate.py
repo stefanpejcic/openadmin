5a5021349ebbab02bea16533088049e1eb274e96
Stefan <stefan@pejcic.rs>
2026-07-08 13:07:20 +0200

    encoded on GITHUB for x86-64  - 2026-07-08 13:07:19

--- modules/api/generate.py
+++ modules/api/generate.py
@@ -4,7 +4,7 @@
 # * OpenAdmin                                                             *
 # * Copyright (c) OpenPanel. All Rights Reserved.                         *
 # * Version: 1.7.65                                                        *
-# * Build Date: 2026-07-08 12:50:12                                       *
+# * Build Date: 2026-07-08 13:07:19                                       *
 # *                                                                       *
 # *************************************************************************
 # *                                                                       *
